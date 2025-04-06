88b1044d1a7b02666fa68df5618382ac6e9fca49
Ennah Lkhlifa <123ennah@gmail.com>
2025-04-06 04:41:49 +0100

    add style

---
+++ QR.js
@@ -0,0 +1,29 @@
+function generateQRCode() {
+    const url = document.getElementById('urlInput').value.trim()
+    const errorText = document.getElementById('error')
+    const print = document.getElementById('print')
+    if (!url || !/^https?:\/\//i.test(url)) {
+        errorText.innerText = 'Please enter a valid URL (e.g., http:// or https://)'
+        return
+    }
+
+    document.getElementById('qrCode').innerHTML = ''
+
+    const canvas = document.createElement('canvas')
+    document.getElementById('qrCode').appendChild(canvas)
+
+    QRCode.toCanvas(canvas, url, function (error) {
+        if (error) {
+            console.error(error)
+            errorText.innerText = 'Error generating QR code'
+        } else {
+            console.log('QR code generated successfully!')
+            errorText.innerText = ''
+            print.style.display = 'block'
+        }
+    })
+}
+
+function hundelPrint() {
+    print()
+}
